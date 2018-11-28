7f87116549c92698eccf8e12e2ed40a7c4f3dd7c
SPhoenixA <sa7zz@virginia.edu>
2018-11-28 16:41:20 -0500

    Instruction Display
    I refined the instruction display and changed around the organization a little bit. Make sure to pull the changes I made before making any changes of your own

--- game.py
+++ game.py
@@ -5,42 +5,73 @@ import gamebox
 camera = gamebox.Camera(800, 600)
 
 #This is the title page
-game_on = False
-instructions = '''
-Welcome to PUSHTOWN!!! Stay in bounds, or die.  Push other players offscreen, move to stay within the scrolling screen and eat apples to make your pushes stronger.
-Whoever has the least deaths when time is up wins.
-Player 1: Press 'w' to jump. Press 'a' to move left. Press 'd' to move right. Press 's' to push.
-Player 2: Press the up arrow key to jump. Press the left arrow key to move left. Press the right arrow key to move right. Press the down arrow key to push.
-'''
+to_draw = []
+player_1 = []
+player_2 = []
+game_state = -1
 
-def draw_title(keys):
-    global game_on
-    if keys:
-        game_on = True
-    keys.clear()
-    to_draw = []
-    title_box = gamebox.from_text(400, 30, 'PUSHTOWN!!!', 'blue', True)
-    to_draw.append(title_box)
+
+def draw_title():
+    instructions = ['''
+    Welcome to PUSHTOWN!!!\n Stay in bounds, or die.\n  Push other players offscreen, move to stay within the scrolling screen and eat apples to make your pushes stronger.
+    Whoever has the least deaths when time is up wins.
+    ''',
+    "Player 1: \nPress 'w' to jump. \nPress 'a' to move left. \nPress 'd' to move right. \nPress 's' to push.",
+    "Player 2: \nPress the up arrow key to jump. \nPress the left arrow key to move left. \nPress the right arrow key to move right. \nPress the down arrow key to push.",
+    "Press space to start."]
+
+    text_height = 100
+    player_title_height = 0
+    for item in instructions[0].split("\n"):
+        title_box = gamebox.from_text(400, text_height, item, 20, "white")
+        to_draw.append(title_box)
+        text_height += 25
+    for item1 in instructions[1].split("\n"):
+        title_1 = gamebox.from_text(200, text_height, item1, 20, "white")
+        to_draw.append(title_1)
+        text_height += 25
+        player_title_height += 25
+
+    text_height -= player_title_height
+    for item2 in instructions[2].split("\n"):
+        title_2 = gamebox.from_text(600, text_height, item2, 20, "white")
+        to_draw.append(title_2)
+        text_height += 25
+
+    to_draw.append(gamebox.from_text(400, text_height+25, instructions[3], 40, "white"))
+
+
+
 #drawing the instructions
-    for line in instructions.split('\n'):
-        to_draw.append(gamebox.from_text(300, 200, line, 30, 'blue'))
-
-    for box in to_draw:
-        camera.draw(box)
 
 #These are the things we need to draw for the game
 # p1 = gamebox.
 # p2 = gamebox.
-background = gamebox.from_color(400, 300, 'green', 800, 600)
+def regular_gameplay():
+
+    background = gamebox.from_color(400, 300, 'green', 800, 600)
 def draw_game(keys):
     to_draw = []
     to_draw.append(background)
-    for box in to_draw:
-        camera.draw(box)
+
 
 #This is how the characters will move
 def tick(keys):
-    to_draw = []
+    global game_state
+    camera.clear("black")
+    if game_state == -1:
+        draw_title()
+        if pygame.K_SPACE in keys:
+            game_state = 0
+
+    if game_state >= 0:
+
+        regular_gameplay()
+
+    game_state += 1
+    for box in to_draw:
+        camera.draw(box)
+    camera.display()
     # if pygame.K_RIGHT in keys:
     #     p2.x += 5
     # if pygame.K_LEFT in keys:
@@ -59,9 +90,8 @@ def tick(keys):
     #     p1.y #attack
 
 
-    camera.display()
 
-gamebox.timer_loop(30, tick)
+gamebox.timer_loop(60, tick)
 
 '''      
 Optional features:
